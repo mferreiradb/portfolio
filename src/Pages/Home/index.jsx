154784c05e263ce8e28c7cf00f4e72d0f6b3668d
Mauricio Ferreira <99357847+mferreiradb@users.noreply.github.com>
2022-09-24 08:45:37 -0300

    Update index.jsx

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,39 +1,149 @@
 import './style.css'
-import Header from "../../components/Header"
-import MinhaFoto from '../../assets/minha-foto.png'
-import Footer from '../../components/Footer'
+import Footer from "../../components/Footer";
+import Header from "../../components/Header";
 
-export default function Home() {
-    const dev = "<JuniorDeveloper />"
+import Sorveteria from '../../assets/sorveteria.png'
+import Pokemon from '../../assets/pokemon.png'
+import HelpDev from '../../assets/HelpDev.png'
+import MeusLinks from '../../assets/meus-links.png'
+import SeuFuturo from '../../assets/SeuFuturo.png'
+import dsMeta from '../../assets/dsMeta.png'
+import Concreta from '../../assets/Concreta.png'
+import Pousada from '../../assets/pousada.png'
 
+export default function Projetos() {
     return (
         <div>
             <Header />
-            <section className="bg-dark-blue-project flex flex-col fundo-home px-5 space-y-5 lg:flex-row lg:justify-around xl:flex-row xl:justify-around 2xl:flex-row 2xl:justify-around">
+            <section className="bg-light-blue-project flex flex-col px-10 space-y-5">
+
                 <div className="flex flex-col justify-center space-y-6 text-center my-28">
-                    <p className="text-slate-300 text-2xl lg:text-4xl">Olá! Tudo bem? Eu me chamo</p>
-                    <h1 className="text-white text-3xl font-bold lg:text-5xl">Mauricio Ferreira</h1>
-                    <code className="text-slate-400 text-2xl lg:text-4xl">{dev}</code>
-                </div>
-                
-                <div className="flex flex-col justify-center">
-                    <code className='text-white text-xl leading-normal lg:text-3xl xl:text-4xl'>
-                        System.out.println("Hello world!");<br />
-                        console.log("Hello world!");<br />
-                        print("Hello world!")
-                    </code>
-                </div>
-            </section>
+                    <h2 className='text-3xl text-white font-semibold'>Projetos responsivos</h2>
+                    <p className="text-white text-xl">Projetos desenvolvidos em um momento no qual eu já possuía mais conhecimento em desenvolvimento Front-End</p>
 
-            <section className="bg-light-blue-project flex flex-col items-center p-5">
+                    <div className="flex flex-col justify-center text-center my-28 xl:flex-row xl:flex-wrap">
 
-                <div className='text-center flex flex-col items-center space-y-6'>
-                    <h2 className='text-3xl text-white font-semibold'>Quem sou eu?</h2>
-                    <div className='text-center flex flex-col items-center space-y-6 xl:flex-row xl:px-5 xl:space-x-96'>
-                        <img src={MinhaFoto} alt='minha-foto' />
-                        <p className='text-white text-lg leading-relaxed'>Meu nome é Mauricio, sou natural de Parnaíba-PI. Tenho formação técnica em Administração, Atendimento ao Cliente e Gestão de Pessoas. Estou me desenvolvendo na área de Tecnologia e busco trabalhar com programação. Possuo experiência com Banco de Dados SQL (MYSQL e Firebird), linguagem Javascript, React Js, React Native, Photoshop, Figma e Pacote Office, além de já ter estudado Python e Java. Estou estudando desenvolvimento FullStack na DevMedia, uma das maiores escolas de programação do mercado.</p>
+                        <div className='flex flex-col items-center border-2 border-blue-900 p-4 space-y-5 xl:w-1/4 xl:m-10 my-10'>
+                            <h3 className='text-2xl text-white font-semibold'>Sorveteria</h3>
+                            <p className="text-white text-lg">Projeto de SPA para uma Sorveteria aplicando o conceito de Mobile First</p>
+                            <ul className='flex flex-col items-center'>
+                                <h4 className='text-lg text-white font-semibold'>Tecnologias utilizadas</h4>
+                                <li type='disc' className='text-sm w-2/4 text-white'>JavaScript</li>
+                                <li type='disc' className='text-sm w-2/4 text-white'>React JSS</li>
+                                <li type='disc' className='text-sm w-2/4 text-white'>TailWind CSS</li>
+                                <li type='disc' className='text-sm w-4/4 text-white'>React-Router-Dom</li>
+                            </ul>
+                            <img src={Sorveteria} className='w-3/4 xl:w-52' />
+                            <a className="text-white text-md border-4 border-blue-900 py-1 px-4 rounded-2xl" href='https://lnkd.in/dVyH2A5n' target='_blank'>Acessar</a>
+                        </div>
+
+                        <div className='flex flex-col items-center border-2 border-blue-900 p-4 space-y-5 xl:w-1/4 xl:m-10 my-10'>
+                            <h3 className='text-2xl text-white font-semibold'>Projeto Help Dev</h3>
+                            <p className="text-white text-lg">Página dedicada a fornecer materiais de consulta para quem está estudando sobre desenvolvidos</p>
+                            <ul className='flex flex-col items-center'>
+                                <h4 className='text-lg text-white font-semibold'>Tecnologias utilizadas</h4>
+                                <li type='disc' className='text-sm w-2/4 text-white'>HTML 5</li>
+                                <li type='disc' className='text-sm w-2/4 text-white'>CSS 3</li>
+                                <li type='disc' className='text-sm w-2/4 text-white'>JavaScript</li>
+                            </ul>
+                            <img src={HelpDev} className='w-3/4 xl:w-52' />
+                            <a className="text-white text-md border-4 border-blue-900 py-1 px-4 rounded-2xl" href='https://mferreiradb.github.io/help-dev/' target='_blank'>Acessar</a>
+                        </div>
+
+
+                        <div className='flex flex-col items-center border-2 border-blue-900 p-4 space-y-5 xl:w-1/4 xl:m-10 my-10'>
+                            <h3 className='text-2xl text-white font-semibold'>Meus Links</h3>
+                            <p className="text-white text-lg">Página contendo meus links principais</p>
+                            <ul className='flex flex-col items-center'>
+                                <h4 className='text-lg text-white font-semibold'>Tecnologias utilizadas</h4>
+                                <li type='disc' className='text-sm w-2/4 text-white'>HTML 5</li>
+                                <li type='disc' className='text-sm w-2/4 text-white'>CSS 3</li>
+                            </ul>
+                            <img src={MeusLinks} className='w-3/4 xl:w-52' />
+                            <a className="text-white text-md border-4 border-blue-900 py-1 px-4 rounded-2xl" href='https://mferreiradb.github.io/meus-links/' target='_blank'>Acessar</a>
+                        </div>
+
+                        <div className='flex flex-col items-center border-2 border-blue-900 p-4 space-y-5 xl:w-1/4 xl:m-10 my-10'>
+                            <h3 className='text-2xl text-white font-semibold'>Adivinhe Seu Futuro</h3>
+                            <p className="text-white text-lg">Página na qual você faz uma pergunta e uma resposta será dada de acordo</p>
+                            <ul className='flex flex-col items-center'>
+                                <h4 className='text-lg text-white font-semibold'>Tecnologias utilizadas</h4>
+                                <li type='disc' className='text-sm w-2/4 text-white'>HTML 5</li>
+                                <li type='disc' className='text-sm w-2/4 text-white'>CSS 3</li>
+                                <li type='disc' className='text-sm w-2/4 text-white'>JavaScript</li>
+                            </ul>
+                            <img src={SeuFuturo} className='w-3/4 xl:w-52' />
+                            <a className="text-white text-md border-4 border-blue-900 py-1 px-4 rounded-2xl" href='https://mferreiradb.github.io/maratona-explorer1-rocketseat/' target='_blank'>Acessar</a>
+                        </div>
+
+                        <div className='flex flex-col items-center border-2 border-blue-900 p-4 space-y-5 xl:w-1/4 xl:m-10 my-10'>
+                            <h3 className='text-2xl text-white font-semibold'>Verificação de vendas</h3>
+                            <p className="text-white text-lg">Projeto fullstack com React e Spring de relatório de vendas por período com envio de sms com dados das vendas</p>
+                            <ul className='flex flex-col items-center'>
+                                <h4 className='text-lg text-white font-semibold'>Tecnologias utilizadas</h4>
+                                <li type='disc' className='text-sm w-2/4 text-white'>TypeScript</li>
+                                <li type='disc' className='text-sm w-2/4 text-white'>React JS</li>
+                                <li type='disc' className='text-sm w-2/4 text-white'>Java</li>
+                                <li type='disc' className='text-sm w-2/4 text-white'>Spring</li>
+                            </ul>
+                            <img src={dsMeta} className='w-3/4 xl:w-52' />
+                            <a className="text-white text-md border-4 border-blue-900 py-1 px-4 rounded-2xl" href='https://www.linkedin.com/posts/mferreiradb_react-reactjs-vendas-activity-6974475209961558016-oLAo?utm_source=share&utm_medium=member_desktop' target='_blank'>Acessar</a>
+                            <p className="text-white text-md">Este projeto não está online devido dificuldades com a Heroku, porém ao clicar em Acessar, você será redirecionado(a) para a minha publicação feita no Linkedin</p>
+                        </div>
+
+                        <div className='flex flex-col items-center border-2 border-blue-900 p-4 space-y-5 xl:w-1/4 xl:m-10 my-10'>
+                            <h3 className='text-2xl text-white font-semibold'>Concreta Serviços de Engenharia</h3>
+                            <p className="text-white text-lg">Página desenvolvida para um cliente que possui uma empresa de engenharia</p>
+                            <ul className='flex flex-col items-center'>
+                                <h4 className='text-lg text-white font-semibold'>Tecnologias utilizadas</h4>
+                                <li type='disc' className='text-sm w-2/4 text-white'>JavaScript</li>
+                                <li type='disc' className='text-sm w-2/4 text-white'>React JSS</li>
+                                <li type='disc' className='text-sm w-2/4 text-white'>TailWind CSS</li>
+                                <li type='disc' className='text-sm w-4/4 text-white'>React-Router-Dom</li>
+                            </ul>
+                            <img src={Concreta} className='w-3/4 xl:w-52' />
+                            <a className="text-white text-md border-4 border-blue-900 py-1 px-4 rounded-2xl" href='https://concreta-arq.vercel.app/' target='_blank'>Acessar</a>
+                        </div>
                     </div>
                 </div>
+
+            </section>
+
+            <section className="bg-dark-blue-project flex flex-col p-5 space-y-5">
+
+                <div className="flex flex-col justify-center space-y-6 text-center my-28">
+                    <h2 className='text-3xl text-white font-semibold'>Projetos não responsivos</h2>
+                    <p className="text-white text-xl">Projetos desenvolvidos nos meus primeiros momentos de aprendizado sobre desenvolvimento Front-End</p>
+
+
+                    <div className="flex flex-col justify-center text-center my-28 xl:flex-row xl:flex-wrap">
+
+                        <div className='flex flex-col items-center border-2 border-blue-900 p-4 space-y-5 xl:w-1/4 xl:m-10 my-10'>
+                            <h3 className='text-2xl text-white font-semibold'>Pokémon</h3>
+                            <p className="text-white text-lg">Projeto de página com o tema Pokémon</p>
+                            <ul className='flex flex-col items-center'>
+                                <h4 className='text-lg text-white font-semibold'>Tecnologias utilizadas</h4>
+                                <li type='disc' className='text-sm w-2/4 text-white'>HTML 5</li>
+                                <li type='disc' className='text-sm w-2/4 text-white'>CSS 3</li>
+                            </ul>
+                            <img src={Pokemon} className='w-3/4 xl:w-52' />
+                            <a className="text-white text-md border-4 border-blue-900 py-1 px-4 rounded-2xl" href='https://mferreiradb.github.io/pokemon-devmedia/' target='_blank'>Acessar</a>
+                        </div>
+
+                        <div className='flex flex-col items-center border-2 border-blue-900 p-4 space-y-5 xl:w-1/4 xl:m-10 my-10'>
+                            <h3 className='text-2xl text-white font-semibold'>Pousada Secreta</h3>
+                            <p className="text-white text-lg">Página de divulgação de pousada</p>
+                            <ul className='flex flex-col items-center'>
+                                <h4 className='text-lg text-white font-semibold'>Tecnologias utilizadas</h4>
+                                <li type='disc' className='text-sm w-2/4 text-white'>HTML 5</li>
+                                <li type='disc' className='text-sm w-2/4 text-white'>CSS 3</li>
+                            </ul>
+                            <img src={Pousada} className='w-3/4 xl:w-52' />
+                            <a className="text-white text-md border-4 border-blue-900 py-1 px-4 rounded-2xl" href='https://mferreiradb.github.io/pousada-secreta-devmedia/' target='_blank'>Acessar</a>
+                        </div>
+                    </div>
+                </div>
+
             </section>
 
             <Footer />
