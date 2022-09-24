a418a267f3b2cd34d7076f2472ec9640be80f0e2
Mauricio Ferreira <99357847+mferreiradb@users.noreply.github.com>
2022-09-24 09:03:13 -0300

    Update index.jsx

--- src/Pages/Projetos/index.jsx
+++ src/Pages/Projetos/index.jsx
@@ -2,7 +2,8 @@ import './style.css'
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
 
-import Pokemon from '../../assets/pokemon.PNG'
+import Sorveteria from '../../assets/sorveteria.png'
+import Pokemon from '../../assets/pokemon.png'
 import HelpDev from '../../assets/HelpDev.png'
 import MeusLinks from '../../assets/meus-links.png'
 import SeuFuturo from '../../assets/SeuFuturo.png'
@@ -22,7 +23,19 @@ export default function Projetos() {
 
                     <div className="flex flex-col justify-center text-center my-28 xl:flex-row xl:flex-wrap">
 
-
+                        <div className='flex flex-col items-center border-2 border-blue-900 p-4 space-y-5 xl:w-1/4 xl:m-10 my-10'>
+                            <h3 className='text-2xl text-white font-semibold'>Sorveteria</h3>
+                            <p className="text-white text-lg">Projeto de SPA para uma Sorveteria aplicando o conceito de Mobile First</p>
+                            <ul className='flex flex-col items-center'>
+                                <h4 className='text-lg text-white font-semibold'>Tecnologias utilizadas</h4>
+                                <li type='disc' className='text-sm w-2/4 text-white'>JavaScript</li>
+                                <li type='disc' className='text-sm w-2/4 text-white'>React JSS</li>
+                                <li type='disc' className='text-sm w-2/4 text-white'>TailWind CSS</li>
+                                <li type='disc' className='text-sm w-4/4 text-white'>React-Router-Dom</li>
+                            </ul>
+                            <img src={Sorveteria} className='w-3/4 xl:w-52' />
+                            <a className="text-white text-md border-4 border-blue-900 py-1 px-4 rounded-2xl" href='https://lnkd.in/dVyH2A5n' target='_blank'>Acessar</a>
+                        </div>
 
                         <div className='flex flex-col items-center border-2 border-blue-900 p-4 space-y-5 xl:w-1/4 xl:m-10 my-10'>
                             <h3 className='text-2xl text-white font-semibold'>Projeto Help Dev</h3>
