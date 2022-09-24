9f6543bda6788730d052e802cc09b2da5b55f35d
Mauricio <mauhmg@gmail.com>
2022-09-23 22:18:04 -0300

    Alteração de cor

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -9,7 +9,7 @@ export default function Home() {
     return (
         <div>
             <Header />
-            <section className="bg-dark-blue-project flex flex-col fundo-home px-5 space-y-5 lg:flex-row lg:justify-around xl:flex-row xl:justify-around 2xl:flex-row 2xl:justify-around">
+            <section className="bg-light-blue-project flex flex-col fundo-home px-5 space-y-5 lg:flex-row lg:justify-around xl:flex-row xl:justify-around 2xl:flex-row 2xl:justify-around">
                 <div className="flex flex-col justify-center space-y-6 text-center my-28">
                     <p className="text-slate-300 text-2xl lg:text-4xl">Olá! Tudo bem? Eu me chamo</p>
                     <h1 className="text-white text-3xl font-bold lg:text-5xl">Mauricio Ferreira</h1>
