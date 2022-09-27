aff1c83176841ce23a7082a1ae5c9eb06aede2a9
Mauricio <mauhmg@gmail.com>
2022-09-27 15:15:58 -0300

    Alteração na posição do FloatButton

--- src/components/FloatButton/index.jsx
+++ src/components/FloatButton/index.jsx
@@ -2,7 +2,7 @@ import KeyboardCapslockIcon from '@mui/icons-material/KeyboardCapslock';
 
 export default function FloatButton() {
     return (
-        <div className='w-12 border-2 border-white fixed z-1 bottom-10 right-20 rounded-full p-2 text-center'>
+        <div className='w-12 border-2 border-white fixed z-1 bottom-10 right-5 xl:right-20 rounded-full p-2 text-center'>
             <a className='text-white' href="#topo"><KeyboardCapslockIcon /></a>
         </div>
     )
