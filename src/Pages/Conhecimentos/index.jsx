91620001439fe701a8fe6de53e8817d582c93262
Mauricio <mauhmg@gmail.com>
2022-09-27 15:11:15 -0300

    Adição do botão flutuante na página Conhecimentos

--- src/Pages/Conhecimentos/index.jsx
+++ src/Pages/Conhecimentos/index.jsx
@@ -1,3 +1,4 @@
+import FloatButton from "../../components/FloatButton";
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
 
@@ -5,12 +6,14 @@ export default function Conhecimentos() {
     return (
         <div>
             <Header />
-            <section className="bg-red-700 flex flex-col items-center p-5">
+            <section id='topo' className="bg-red-700 flex flex-col items-center p-5">
                 <div className='text-center flex flex-col items-center space-y-6'>
                     <h2 className='text-3xl text-white font-semibold'>Quem sou eu?</h2>
                     <p className='text-white text-lg leading-relaxed'>Meu nome é Mauricio, sou natural de Parnaíba-PI. Tenho formação técnica em Administração, Atendimento ao Cliente e Gestão de Pessoas. Estou me desenvolvendo na área de Tecnologia e busco trabalhar com programação. Possuo experiência com Banco de Dados SQL (MYSQL e Firebird), linguagem Javascript, React Js, React Native, Photoshop, Figma e Pacote Office, além de já ter estudado Python e Java. Estou estudando desenvolvimento FullStack na DevMedia, uma das maiores escolas de programação do mercado.</p>
                 </div>
             </section>
+
+            <FloatButton />
 
             <Footer />
         </div>
