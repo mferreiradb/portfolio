cc43ac31314621153c9fc73923461d5d55c67045
Mauricio <mauhmg@gmail.com>
2022-09-27 11:53:29 -0300

    Alteração de alinhamento do Footer e da seção Sobre

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -15,7 +15,7 @@ export default function Home() {
                     <h1 className="text-white text-3xl font-bold lg:text-5xl">Mauricio Ferreira</h1>
                     <code className="text-slate-400 text-2xl lg:text-4xl">{dev}</code>
                 </div>
-                
+
                 <div className="flex flex-col justify-center">
                     <code className='text-white text-xl leading-normal lg:text-3xl xl:text-4xl'>
                         System.out.println("Hello world!");<br />
@@ -25,15 +25,18 @@ export default function Home() {
                 </div>
             </section>
 
-            <section className="bg-light-blue-project flex flex-col items-center p-5">
-
-                <div className='text-center flex flex-col items-center space-y-6'>
-                    <h2 className='text-3xl text-white font-semibold'>Quem sou eu?</h2>
-                    <div className='text-center flex flex-col items-center space-y-6 xl:flex-row xl:px-5 xl:space-x-96'>
+            <section className="bg-light-blue-project flex flex-col items-center p-5 xl:flex xl:justify-center">
+                
+                    <div className='text-center flex flex-col items-center justify-center space-y-6 xl:flex-row xl:px-5 xl:justify-center xl:space-x-10'>
                         <img src={MinhaFoto} alt='minha-foto' />
-                        <p className='text-white text-lg leading-relaxed'>Meu nome é Mauricio, sou natural de Parnaíba-PI. Tenho formação técnica em Administração, Atendimento ao Cliente e Gestão de Pessoas. Estou me desenvolvendo na área de Tecnologia e busco trabalhar com programação. Possuo experiência com Banco de Dados SQL (MYSQL e Firebird), linguagem Javascript, React Js, React Native, Photoshop, Figma e Pacote Office, além de já ter estudado Python e Java. Estou estudando desenvolvimento FullStack na DevMedia, uma das maiores escolas de programação do mercado.</p>
+
+                        <div className='text-center flex flex-col items-center space-y-6 xl:w-1/2'>
+
+                            <h2 className='text-3xl text-white font-semibold'>Quem sou eu?</h2>
+
+                            <p className='text-white text-lg leading-relaxed'>Meu nome é Mauricio, sou natural de Parnaíba-PI. Tenho formação técnica em Administração, Atendimento ao Cliente e Gestão de Pessoas. Estou me desenvolvendo na área de Tecnologia e busco trabalhar com programação. Possuo experiência com Banco de Dados SQL (MYSQL e Firebird), linguagem Javascript, React Js, React Native, Photoshop, Figma e Pacote Office, além de já ter estudado Python e Java. Estou estudando desenvolvimento FullStack na DevMedia, uma das maiores escolas de programação do mercado.</p>
+                        </div>
                     </div>
-                </div>
             </section>
 
             <Footer />
--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -7,26 +7,26 @@ import CallIcon from '@mui/icons-material/Call';
 const Footer = () => (
     <footer className='space-y-5 pt-16 pb-5 flex flex-col items-center text-center border-t-2 bg-dark-blue-project border-blue-900'>
 
-        <div className="space-y-5">
+        <div className="">
             <h2 className='text-white font-bold text-xl mb-2'>CONTATO</h2>
 
-            <div className='flex flex-col items-center space-y-3' id='contato'>
-                <div className='flex space-x-4'>
+            <div className='flex flex-col items-center xl:flex-row xl:space-x-10' id='contato'>
+                <div className='flex space-x-2 my-1'>
                     <EmailIcon className='text-white' />
                     <a href='mailto:mauhmg@gmail.com<' target="_blank" className='underline text-white'>mauhmg@gmail.com</a>
                 </div>
 
-                <div className='flex space-x-4'>
+                <div className='flex space-x-2 my-1'>
                     <WhatsAppIcon className='text-white' />
                     <a href="https://api.whatsapp.com/send?phone=5586995256729" target="_blank" className='underline text-white'>(86) 9 9525-6729</a>
                 </div>
 
-                <div className='flex space-x-4'>
+                <div className='flex space-x-2 my-1'>
                     <CallIcon className='text-white' />
                     <a href="tel:86981033127" target="_blank" className='underline text-white'>(86) 9 8103-3127</a>
                 </div>
 
-                <div className='flex space-x-4'>
+                <div className='flex space-x-2 my-1'>
                     <LinkedInIcon className='text-white' />
                     <a href="https://www.linkedin.com/in/mferreiradb/" target="_blank" className='underline text-white'>/mferreiradb</a>
                 </div>
