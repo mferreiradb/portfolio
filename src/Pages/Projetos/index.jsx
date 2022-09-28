85deeca2bc14bca0306f702acf329b2fa0ff4c33
Mauricio <mauhmg@gmail.com>
2022-09-28 11:59:31 -0300

    Alteração na home

--- src/Pages/Projetos/index.jsx
+++ src/Pages/Projetos/index.jsx
@@ -24,7 +24,7 @@ export default function Projetos() {
 
                     <div className="flex flex-col justify-center text-center my-28 xl:flex-row xl:flex-wrap">
 
-                        <div className='flex flex-col items-center border-2 border-blue-900 p-4 space-y-5 xl:w-1/4 xl:m-10 my-10'>
+                        <div className='flex flex-col items-center border-2 border-blue-900 p-4 space-y-5 xl:w-1/4 xl:m-5 my-5'>
                             <h3 className='text-2xl text-white font-semibold'>Sorveteria</h3>
                             <p className="text-white text-lg">Projeto de SPA para uma Sorveteria aplicando o conceito de Mobile First</p>
                             <ul className='flex flex-col items-center'>
@@ -38,7 +38,7 @@ export default function Projetos() {
                             <a className="text-white text-md border-4 border-blue-900 py-1 px-4 rounded-2xl" href='https://lnkd.in/dVyH2A5n' target='_blank'>Acessar</a>
                         </div>
 
-                        <div className='flex flex-col items-center border-2 border-blue-900 p-4 space-y-5 xl:w-1/4 xl:m-10 my-10'>
+                        <div className='flex flex-col items-center border-2 border-blue-900 p-4 space-y-5 xl:w-1/4 xl:m-5 my-5'>
                             <h3 className='text-2xl text-white font-semibold'>Projeto Help Dev</h3>
                             <p className="text-white text-lg">Página dedicada a fornecer materiais de consulta para quem está estudando sobre desenvolvidos</p>
                             <ul className='flex flex-col items-center'>
@@ -52,7 +52,7 @@ export default function Projetos() {
                         </div>
 
 
-                        <div className='flex flex-col items-center border-2 border-blue-900 p-4 space-y-5 xl:w-1/4 xl:m-10 my-10'>
+                        <div className='flex flex-col items-center border-2 border-blue-900 p-4 space-y-5 xl:w-1/4 xl:m-5 my-5'>
                             <h3 className='text-2xl text-white font-semibold'>Meus Links</h3>
                             <p className="text-white text-lg">Página contendo meus links principais</p>
                             <ul className='flex flex-col items-center'>
@@ -64,7 +64,7 @@ export default function Projetos() {
                             <a className="text-white text-md border-4 border-blue-900 py-1 px-4 rounded-2xl" href='https://mferreiradb.github.io/meus-links/' target='_blank'>Acessar</a>
                         </div>
 
-                        <div className='flex flex-col items-center border-2 border-blue-900 p-4 space-y-5 xl:w-1/4 xl:m-10 my-10'>
+                        <div className='flex flex-col items-center border-2 border-blue-900 p-4 space-y-5 xl:w-1/4 xl:m-5 my-5'>
                             <h3 className='text-2xl text-white font-semibold'>Adivinhe Seu Futuro</h3>
                             <p className="text-white text-lg">Página na qual você faz uma pergunta e uma resposta será dada de acordo</p>
                             <ul className='flex flex-col items-center'>
@@ -77,7 +77,7 @@ export default function Projetos() {
                             <a className="text-white text-md border-4 border-blue-900 py-1 px-4 rounded-2xl" href='https://mferreiradb.github.io/maratona-explorer1-rocketseat/' target='_blank'>Acessar</a>
                         </div>
 
-                        <div className='flex flex-col items-center border-2 border-blue-900 p-4 space-y-5 xl:w-1/4 xl:m-10 my-10'>
+                        <div className='flex flex-col items-center border-2 border-blue-900 p-4 space-y-5 xl:w-1/4 xl:m-5 my-5'>
                             <h3 className='text-2xl text-white font-semibold'>Verificação de vendas</h3>
                             <p className="text-white text-lg">Projeto fullstack com React e Spring de relatório de vendas por período com envio de sms com dados das vendas</p>
                             <ul className='flex flex-col items-center'>
@@ -92,7 +92,7 @@ export default function Projetos() {
                             <p className="text-white text-md">Este projeto não está online devido dificuldades com a Heroku, porém ao clicar em Acessar, você será redirecionado(a) para a minha publicação feita no Linkedin</p>
                         </div>
 
-                        <div className='flex flex-col items-center border-2 border-blue-900 p-4 space-y-5 xl:w-1/4 xl:m-10 my-10'>
+                        <div className='flex flex-col items-center border-2 border-blue-900 p-4 space-y-5 xl:w-1/4 xl:m-5 my-5'>
                             <h3 className='text-2xl text-white font-semibold'>Concreta Serviços de Engenharia</h3>
                             <p className="text-white text-lg">Página desenvolvida para um cliente que possui uma empresa de engenharia</p>
                             <ul className='flex flex-col items-center'>
@@ -119,7 +119,7 @@ export default function Projetos() {
 
                     <div className="flex flex-col justify-center text-center my-28 xl:flex-row xl:flex-wrap">
 
-                        <div className='flex flex-col items-center border-2 border-blue-900 p-4 space-y-5 xl:w-1/4 xl:m-10 my-10'>
+                        <div className='flex flex-col items-center border-2 border-blue-900 p-4 space-y-5 xl:w-1/4 xl:m-5 my-5'>
                             <h3 className='text-2xl text-white font-semibold'>Pokémon</h3>
                             <p className="text-white text-lg">Projeto de página com o tema Pokémon</p>
                             <ul className='flex flex-col items-center'>
@@ -131,7 +131,7 @@ export default function Projetos() {
                             <a className="text-white text-md border-4 border-blue-900 py-1 px-4 rounded-2xl" href='https://mferreiradb.github.io/pokemon-devmedia/' target='_blank'>Acessar</a>
                         </div>
 
-                        <div className='flex flex-col items-center border-2 border-blue-900 p-4 space-y-5 xl:w-1/4 xl:m-10 my-10'>
+                        <div className='flex flex-col items-center border-2 border-blue-900 p-4 space-y-5 xl:w-1/4 xl:m-5 my-5'>
                             <h3 className='text-2xl text-white font-semibold'>Pousada Secreta</h3>
                             <p className="text-white text-lg">Página de divulgação de pousada</p>
                             <ul className='flex flex-col items-center'>
