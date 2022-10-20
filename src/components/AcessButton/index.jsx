a6bf27537d936b5a694740cb54857f1d7259d727
Mauricio <mauhmg@gmail.com>
2022-10-20 09:59:12 -0300

    Alteração na sintaxe de props do componente AcessButton

--- src/components/AcessButton/index.jsx
+++ src/components/AcessButton/index.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
-const AcessButton = (props) => {
+const AcessButton = ({url}) => {
 	return (
-		<a className="text-white hover:bg-dark-blue-project text-md border-4 border-blue-900 py-1 px-4 rounded-2xl" href={props.url} target='_blank' rel="noreferrer">Acessar</a>
+		<a className="text-white hover:bg-dark-blue-project text-md border-4 border-blue-900 py-1 px-4 rounded-2xl" href={url} target='_blank' rel="noreferrer">Acessar</a>
 	);
 };
 export default AcessButton;
