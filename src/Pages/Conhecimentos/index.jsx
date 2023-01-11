e79a7027590e939ebf041917b61f689dc954abaa
Mauricio <mauhmg@gmail.com>
2023-01-11 14:57:29 -0300

    Alteração na ordem da lista de Adicionais

--- src/Pages/Conhecimentos/index.jsx
+++ src/Pages/Conhecimentos/index.jsx
@@ -122,9 +122,9 @@ export const Conhecimentos = () => {
 
 					<ul className='flex flex-col items-center space-y-1'>
 						<li type='disc' className='text-sm w-7/8 text-white'>MVC</li>
-						<li type='disc' className='text-sm w-7/8 text-white'>SOLID</li>
-						<li type='disc' className='text-sm w-7/8 text-white'>REST</li>
 						<li type='disc' className='text-sm w-7/8 text-white'>POO</li>
+						<li type='disc' className='text-sm w-7/8 text-white'>REST</li>
+						<li type='disc' className='text-sm w-7/8 text-white'>SOLID</li>
 						<li type='disc' className='text-sm w-7/8 text-white'>Sequelize</li>
 					</ul>
 				</Card>
